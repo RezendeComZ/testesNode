731d3d1a4566d0cb42df8924a13d18f9a8365c7b
Gabriel Rezende <g@brielrezen.de>
2021-05-08 14:22:12 -0300

    atualizando em table

--- stats.js
+++ stats.js
@@ -1,13 +1,32 @@
 const os = require('os');
+// console.log(os.cpus())
 
 const {freemem, totalmem } = os
 
-console.log(os.cpus())
+function StatusMemoria() {
+  const memLivre = parseInt(freemem() / 1024 / 1024);
+  const memTotal = parseInt(totalmem() / 1024 / 1024)
+  const percentLivre = parseInt(memLivre / memTotal * 100)
+  const percentUsada = parseInt(((memTotal - memLivre) / memTotal) * 100)
+  console.log('Memória livre: ' + memLivre + " MB")
+  console.log('Memória total: ' + memTotal + " MB")
+  console.log('Porcentagem de memória usada: ' + percentUsada + "%")
+  console.log('Porcentagem de memória livre: ' + percentLivre + "%")
+  
+  const stats = {
+    livre: `${memLivre} MB`,
+    total: `${memTotal} MB`,
+    usada: `${percentUsada}%`
+  }
+  
+  console.clear()
+  console.table(stats);
+
+}
+
+StatusMemoria();
 
 // Memória
-const memLivre = parseInt(freemem() / 1024 / 1024);
-const memTotal = parseInt(totalmem() / 1024 / 1024)
-console.log('Memória livre: ' + memLivre + " MB")
-console.log('Memória total: ' + memTotal + " MB")
-console.log('Porcentagem de memória usada: ' + parseInt(((memTotal - memLivre) / memTotal) * 100)  + "%")
-console.log('Porcentagem de memória livre: ' + parseInt(memLivre / memTotal * 100) + "%")
+setInterval(() => {
+  StatusMemoria()
+}, 1000)
